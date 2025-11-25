606ebf7577e835c59dcc9f6bad5bcbbbfc218c1b
harshfootballer18-svg <harshfootballer18@gmail.com>
2025-11-25 21:11:49 +0530

    Add files via upload

---
+++ fruity info/first.js
@@ -0,0 +1,22 @@
+document.querySelector("#APPLE").addEventListener("click", function () {
+    window.location.href = "apple.html";
+});
+document.querySelector("#BANANA").addEventListener("click", function () {
+    window.location.href = "banana.html";
+});
+document.querySelector("#KIWI").addEventListener("click", function () {
+    window.location.href = "kiwi.html";
+});document.querySelector("#GUAVA").addEventListener("click", function () {
+    window.location.href = "guava.html";
+});
+document.querySelector("#POTATO").addEventListener("click", function () {
+  window.location.href = "potato.html";
+});
+document.querySelector("#ONION").addEventListener("click", function () {
+    window.location.href = "onion.html";
+});document.querySelector("#CAPSICUM").addEventListener("click", function () {
+    window.location.href = "capsicum.html";
+});
+document.querySelector("#CAULIFLOWER").addEventListener("click", function () {
+    window.location.href = "cauliflower.html";
+});
